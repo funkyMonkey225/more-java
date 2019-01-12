cd37390485d5d588965465868d154c46aaa9aaaa
Mac User <sabbey37@gmail.com>
2019-01-12 17:06:30 -0500

    More hello

--- hello2/src/Hello.java
+++ hello2/src/Hello.java
@@ -3,5 +3,8 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello, you!");
+        int myFirstNumber = 5;
+
+        System.out.println(myFirstNumber);
     }
 }
