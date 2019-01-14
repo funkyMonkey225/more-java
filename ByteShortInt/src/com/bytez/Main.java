ff28d7400207ccdc5a445189382aef76155f5920
Mac User <sabbey37@gmail.com>
2019-01-13 21:09:02 -0500

    Qualz

--- ByteShortInt/src/com/bytez/Main.java
+++ ByteShortInt/src/com/bytez/Main.java
@@ -45,6 +45,7 @@ public class Main {
     public static void printYearsAndDays(long minutes) {
         if(minutes < 0) {
             System.out.println("Invalid Value");
+            return;
         }
 
         long days = minutes / (60 * 24);
@@ -54,5 +55,16 @@ public class Main {
         System.out.println(minutes + " min = " + years + " y and " + days + " d");
     }
 
+    public static void printEqual(int one, int two, int three) {
+        if(one < 0 || two < 0 || three < 0) {
+            System.out.println("Invalid Value");
+        } else if (one == two && one == three && two == three) {
+            System.out.println("All numbers are equal");
+        } else if (one != two && one != three && two != three) {
+            System.out.println("All numbers are different");
+        } else {
+            System.out.println("Neither all are equal or different");
+        }
 
+    }
 }
