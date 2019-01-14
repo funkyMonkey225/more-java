fb1d2f64026c1003717b8604052fdb9f5905821b
Mac User <sabbey37@gmail.com>
2019-01-13 20:54:25 -0500

    Areaah

--- ByteShortInt/src/com/bytez/Main.java
+++ ByteShortInt/src/com/bytez/Main.java
@@ -7,7 +7,10 @@ public class Main {
 
     public static void main(String[] args) {
         int zero = 0 / 1024;
-        System.out.println(hasTeen(1, 11, 12));
+        System.out.println(area(5.0));
+        System.out.println(area(-1));
+        System.out.println(area(5.0, 4.0));
+        System.out.println(area(-1.0, 4.0));
     }
 
 
@@ -22,6 +25,22 @@ public class Main {
 
     public static boolean hasTeen(int one, int two, int three) {
         return ((one > 12 && one < 20) || (two > 12 && two < 20) || (three > 12 && three < 20));
+    }
+
+    public static double area(double radius) {
+        if(radius < 0) {
+            return -1.0;
+        }
+
+        return radius * radius * 3.14159;
+    }
+
+    public static double area(double x, double y) {
+        if(x < 0 || y < 0) {
+            return -1.0;
+        }
+
+        return x * y;
     }
 
 
