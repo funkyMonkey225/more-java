78f64dd09b8092dcfe8e614b15458345b01ccbf6
Mac User <sabbey37@gmail.com>
2019-01-24 19:45:29 -0500

    purrfect

--- ByteShortInt/src/com/bytez/Main.java
+++ ByteShortInt/src/com/bytez/Main.java
@@ -26,6 +26,24 @@ public class Main {
         return false;
     }
 
+    public static boolean isPerfectNumber(int number) {
+        if(number < 1) {
+            return false;
+        }
+
+        int sum = 0;
+        for(int i=1; i < number; i++) {
+            if(number % i == 0) {
+                sum += i;
+            }
+        }
+        if(sum == number) {
+            return true;
+        }
+
+        return false;
+    }
+
     public static void printFactors(int number) {
         if(number < 1) {
             System.out.println("Invalid Value");
